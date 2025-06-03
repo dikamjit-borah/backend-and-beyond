6c013df23fde6bbc7b4b02e6af0afd461118827e
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2025-06-03 15:43:04 +0530

    init gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `backend-and-beyond`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag"]
+};
